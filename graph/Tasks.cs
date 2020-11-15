a35ad970ee5f41e346c50b39ceee512275e67ecd
Artem Amangaliev <amon2599@gmail.com>
2020-11-16 00:00:18 +0400

    add one task

--- graph/Tasks.cs
+++ graph/Tasks.cs
@@ -21,13 +21,14 @@ namespace graph
 
             string digit = Console.ReadLine();
             Console.Clear();
+            string u = "";
             switch (digit)
-            {
+            { 
                 case "1":
                     Console.WriteLine("la\n" +
                                       "20) Вывести все вершины орграфа, не смежные с данной.");
                     Console.Write("Введите вершину:  ");
-                    string u = Console.ReadLine();
+                    u = Console.ReadLine();
                     var resStr = g.GetLabelNodes().Except(g.GetLabelNeighborsNode(u));
                     foreach (var a in resStr)
                         Console.Write($"{a}, ");
@@ -36,6 +37,13 @@ namespace graph
                 case "2":
                     Console.WriteLine("la\n" +
                                       "1) Для данной вершины орграфа вывести все «выходящие» соседние вершины.");
+                    Console.Write("Введите вершину:  ");
+                    u = Console.ReadLine();
+                    if (g.Directed == true)
+                    {
+                        foreach (var a in g.GetLabelNeighborsNode(u))
+                            Console.Write($"{a}, ");
+                    }
                     break;
                 case "3":
                     break;
