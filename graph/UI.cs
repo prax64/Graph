adfea6e6aeb63e6baed2f043271172a23bbc95ec
Artem Amangaliev <amon2599@gmail.com>
2020-11-17 01:30:08 +0400

    fix bug

--- graph/UI.cs
+++ graph/UI.cs
@@ -11,9 +11,22 @@ namespace graph
 {
     public class UI
     {
-        public static void MainMenu(Graph.Graph g)
+        private List<Graph.Graph> graphs;
+        
+        private protected List<Graph.Graph> Graphs
+        {
+            get => graphs;
+        }
+        private protected UI(){}
+        public UI(List<Graph.Graph> graphs)
+        {
+            this.graphs = graphs;
+        }
+        public void MainMenu()
         {
             Console.Clear();
+            Console.WriteLine($"Загружено {graphs.Count} для работы.");
+            
             Console.WriteLine("Введите 1, для вывода списока смежности");
             
             Console.WriteLine("Введите 2, для удаления вершины");
@@ -29,51 +42,116 @@ namespace graph
             switch (digit)
             {
                 case "1":
-                    Console.WriteLine(g.print());
-                    GoToMainMenu(g);
+                    Console.Clear();
+                    PrintGraphSelection();
+                    try
+                    {
+                        Console.WriteLine(graphs[GraphSelection() -1].Print());
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        goto default;
+                    }
+                    GoToMainMenu();
                     break;
                 case "2":
+                    Console.Clear();
+                    PrintGraphSelection();
+                    int numberDelTmp = GraphSelection();
                     Console.WriteLine("Введите имя вершины которую необходимо удалить:");
                     string s = Console.ReadLine();
-                    g.DelNode(s);
-                    GoToMainMenu(g);
+                    try
+                    {
+                        graphs[numberDelTmp -1].DelNode(s);
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        goto default;
+                    }
+                    GoToMainMenu();
                     break;
                 case "3":
+                    Console.Clear();
+                    PrintGraphSelection();
+                    int numberDel = GraphSelection();
                     Console.WriteLine("Введите имя вершины из которой удаляем ребро:");
                     string u = Console.ReadLine();
                     Console.WriteLine("Введите имя вершины которую необходимо удалить:");
                     string v = Console.ReadLine();
-                    Console.WriteLine("Введите вес данного ребра:");
-                    int weigth = int.Parse(Console.ReadLine());
-                    g.DelEdge(u,v,weigth);
-                    GoToMainMenu(g);
+                    try
+                    {
+                        graphs[numberDel -1].DelEdge(u,v);
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        goto default;
+                    }
+                    GoToMainMenu();
                     break;
                 case "4":
-                    Tasks.MenuTasks(g);
+                    Console.Clear();
+                    PrintGraphSelection();
+                    Tasks t = new Tasks();
+                    try
+                    {
+                        t.MenuTasks(graphs[GraphSelection() -1]);
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        goto default;
+                    }
+                    MainMenu();
                     break;
                 case "5":
                     break;
                 default:
                     Console.WriteLine("Не верный формат ввода. Введите цифру заново.");
-                    GoToMainMenu(g);
+                    GoToMainMenu();
                     break;
             }
         }
 
-        private static void GoToMainMenu(Graph.Graph g)
+        private void GoToMainMenu()
         {
             Console.WriteLine("\nВведите 1, для перехода в главное меню:");
             string tmp = Console.ReadLine();
             if (tmp == "1")
-                MainMenu(g);
+                MainMenu();
             else
-                GoToMainMenu(g);
+                GoToMainMenu();
         }
-        public static void PrintEdge(Graph.Graph g)
+
+        private void PrintGraphSelection()
         {
-            foreach (var a in g.getEdge() )
+            for (int i = 0; i < graphs.Count; i++)
             {
-                Console.WriteLine(a);
+                Console.WriteLine($"Введите {i+1}, для работы с графом {i+1}");
+            }
+        }
+
+        private int GraphSelection()
+        {
+            try
+            {
+                int numberGraph = int.Parse(Console.ReadLine());
+                return numberGraph;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Неверный формат ввода!!!");
+                GoToMainMenu();
+            }
+
+            return -1;
+        }
+        public void PrintEdge()
+        {
+            foreach (var g in graphs)
+            {
+                foreach (var a in g.GetEdge() )
+                {
+                    Console.WriteLine(a);
+                }
             }
         }
     }
